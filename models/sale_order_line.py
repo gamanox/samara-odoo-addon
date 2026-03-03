2ffedcf597a25d539baaa8ec7b4113116131515f
gamanox <will.alvarez@gmail.com>
2026-03-03 16:58:38 -0600

    Enhance intention form to support multiple intention types and messages; update cart logic to handle new structure

--- models/sale_order_line.py
+++ models/sale_order_line.py
@@ -1,4 +1,4 @@
-from odoo import models, fields, api
+from odoo import models, fields
 
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
@@ -10,11 +10,13 @@ class SaleOrder(models.Model):
 
     def action_confirm(self):
         res = super().action_confirm()
+        intention_model = self.env["parish.intention"].sudo()
         for order in self:
             for line in order.order_line:
+                linked_intentions = intention_model.search([("sol_id", "=", line.id)])
+                target_intentions = linked_intentions
                 if line.parish_intention_id:
-                    try:
-                        line.parish_intention_id.sudo().write({'state': 'paid'})
-                    except Exception:
-                        pass
+                    target_intentions |= line.parish_intention_id.sudo()
+                if target_intentions:
+                    target_intentions.write({"state": "paid"})
         return res
